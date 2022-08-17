6f24e15005dc6f4f984990d671b55713ffa10efe
claude martinot <claudemartinot@orange.fr>
2022-08-17 15:49:03 +0200

    yelp

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:index, :new, :create, :show]
+  end
+  # new et create nested
+
+  # GET "restaurants"
+
+  # GET "restaurants/new"
+  # POST "restaurants"
+
+  # GET "restaurants/38"
+
+  # GET "restaurants/38/reviews/new*** NESTED"
+  # POST "restaurants/38/reviews"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Creating restaurants..."
+banana_me = {name: "Banana me", address: "London", phone_number: "02567895", category: "french"}
+zenazia = {name: "Zenazia", address: "Toulouse", phone_number: "0561227584", category: "chinese"}
+pizzaia = {name: "Pizzaia", address: "Paris", phone_number: "01224861", category: "italian"}
+waffle_fries = {name: "Waffle ans fries", address: "Bruxelles", phone_number: "78522239", category: "belgian"}
+canaille_club = {name: "Canaille club", address: "Toulouse", phone_number: "06281045", category: "french"}
+
+
+[banana_me, zenazia, pizzaia, waffle_fries, canaille_club].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
